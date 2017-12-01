cf7f337f0ab959baf9f73b05a5dd5b138bd28f7c
JuLiaoyuan <stu0615@163.com>
2017-12-01 18:56:46 +0800

    note:1201

--- Swift/Playground/Chapter4.playground/Contents.swift
+++ Swift/Playground/Chapter4.playground/Contents.swift
@@ -9,7 +9,7 @@ import UIKit
 
 // 集合的可变行
 // Swift 中，集合是否可变，很容易被知道，如果你创建时声明是常量(let)，那么集合就是不可变的。
-//如果你创建时声明是变量(var)，那么集合就是可变的。
+// 如果你创建时声明是变量(var)，那么集合就是可变的。
 var mutableArray = Array<Any>() // 可变数组
 let immutableArray = Array<Any>() // 不可变数组
 
@@ -32,7 +32,7 @@ var shoppingList = ["Eggs","Milk"]
 
 // 数组的数量
 var count = shoppingList.count
-// 数组是否为空 Bool 类型
+// 数组是否为空, Bool 类型
 let empty = shoppingList.isEmpty
 // 数组中添加数据
 shoppingList.append("Flour")
@@ -41,7 +41,7 @@ shoppingList += ["Baking Powder"] // 使用 += 可以获得同样的结果
 var firstItem = shoppingList[0]
 // 改变某个已有索引值对应的数据值
 shoppingList[0] = "Six eggs"
-// 改变一些列数据值
+// 改变一系列数据值
 shoppingList[2..<3] = ["Bananas","Apples","Orange"]
 // 插入数据
 shoppingList.insert("Maple Syrup", at: 0)
@@ -60,12 +60,86 @@ for (index, value) in shoppingList.enumerated() {
 // 相等的对象，哈希值也必须相同，比如 a == b ，则 a.hashValue == b.hashValue
 // Swift 的所有基本类型(String、Int、Double、Bool)都是可哈希化的。
 
+// 初始化一个空的集合
+var letters = Set<Character>()
+// 插入一个值
+letters.insert("a")
+// 和数组一样，Set 也可以根据上下文自动推断集合的类型
+letters = [] // 这里 letter 还是 Character 的类型
+// 使用字面量创建集合
+var favoriteGenres0: Set<String> = ["Rock","Classical","Hip Hop"]
+// Set 类型不能从数组字面量倍单独推断出来，所以必须要显示声明，但是，Set 内的元素类型可以被自动推断出来，所以可以简化：
+var favoriteGenres: Set = ["Rock","Classical","Hip Hop"]
+// 集合的元素数量
+favoriteGenres.count
+// 判断集合是否为空
+favoriteGenres.isEmpty
+// 移除某一个元素 并且，如果移除成功，会返回这个被移出的元素，移除失败的话，返回 nil
+favoriteGenres.remove(at: favoriteGenres.startIndex)
+favoriteGenres.remove("Rock")
+favoriteGenres.remove("Rock") // 删除失败
+// 检查集合是否包含一个特定的值
+favoriteGenres.contains("Classical")
+// 遍历集合
+for genre in favoriteGenres0 {
+     print(genre)
+}
+// 对集合进行有序输出
+for genre in favoriteGenres0.sorted() {
+    print(genre)
+}
+
+// 集合的操作
+let oddDigits: Set = [1,3,5,7,9]
+let evenDigits: Set = [0,1,2,4,6,8]
+// 集合的交集(intersection)
+oddDigits.intersection(evenDigits).sorted()
+// 集合的并集(union)
+oddDigits.union(evenDigits).sorted()
+// 集合的对称差(symmetricDifference)， 在集合论里面，对称差的计算方式是： (A∪B) - (A∩B)
+oddDigits.symmetricDifference(evenDigits).sorted()
+// 集合的相对差(subtracting) A - B
+oddDigits.subtracting(evenDigits).sorted()
+let subSet: Set = [1,2,3,4,5]
+let superSet: Set = [1,2,3,4,5,6,7,8]
+
+// 集合的相等， ==
+subSet == superSet
+// 集合的子集
+superSet.isSuperset(of: subSet)
+subSet.isSubset(of: superSet)
+// 集合的真子集
+subSet.isStrictSubset(of: superSet)
+superSet.isStrictSuperset(of: subSet)
+// 集合是否没有有交集
+subSet.isDisjoint(with: superSet) // 两者有交集
 
 
+// 字典
+// 字典的 key 必须遵循 Hashable 协议
 
+// 创建一个空字典
+var namesOfInt = [Int:String]()
+// 同数组一样，它可以根据上下文自动推断字典类型
+namesOfInt = [:] // 仍为 Int: String 类型的字典
 
-
-
-
-
-
+// 使用字面量创建字典
+var airports0: [String: String] = ["YYZ":"Toronto Pearson","DUB":"Dublin"]
+// 自动推断
+var airport = ["YYZ":"Toronto Pearson","DUB":"Dublin"]
+// 字典的数量
+airport.count
+// 字典是否为空
+airport.isEmpty
+// 使用 key 访问/修改值
+airport["YYZ"] = "HongQiao"
+airport["YYZ"]
+airport.updateValue("Pudong", forKey: "YYZ") // 返回的是更新之前的原值
+airport.updateValue("HongQiao", forKey: "SH")
+// 移除某个元素
+airport["SH"] = nil // 直接置 nil 来移除
+airport.removeValue(forKey: "YYZ");
+// 遍历
+for (airportCode, airpotName) in airports0 {
+    print("\(airportCode):\(airpotName)")
+}
---
+++ Swift/Playground/Chapter5.playground/Contents.swift
@@ -0,0 +1,76 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+// 控制流
+// while if guard switch for-in
+
+// switch
+// Swift 中 switch 语句不会贯穿，即不需要使用 break 。如果想要贯穿，使用 fallthrough
+// switch 必须完整。
+
+let anotherChar: Character = "a"
+switch anotherChar {
+case "a": break
+case "A":
+    print("A")
+default:
+    print("else")
+}
+
+// case 分支的模式也可以是一个值的区间
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+
+var naturalCount: String
+
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "serveral"
+default:
+    naturalCount = "many"
+}
+print("there are \(naturalCount)\(countedThings)")
+// switch 匹配元组
+// 元组中的元素可以是值，也可以是区间。 用下划线(_)来匹配所有可能的值
+
+// 值绑定
+// case 分支允许将匹配的值绑定到一个临时的常量和变量，并且在 case 分支体内使用 -- 这种行为被称为值绑定
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+    print(x)
+case (0, let y):
+    print(y)
+case let(x,y):
+    print(x + y)
+}
+
+// where
+// case 分支可以使用 where 语句来判断额外的条件
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let(x, y) where x == y:
+    print("x == y")
+case let(x, y) where x > y:
+    print("x > y")
+case let(x, y) where x < y:
+    print("x < y")
+default:
+    break
+}
+
+// 符合匹配
+// 当多个条件可以使用同一种方法来处理时，可以将这集中可能放在同一个 case 后面，用逗号隔开
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0),(0, let distance):
+    print("\(distance)")
+default:
+    print("not on an axis")
+}
+
+
